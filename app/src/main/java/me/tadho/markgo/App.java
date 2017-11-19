cbacf5206cbb705f57065dcf730be878b6532548
td0 <tadho@me.com>
2017-11-20 00:24:52 +0700

    add LeakCanary
    Signed-off-by: td0 <tadho@me.com>

--- app/src/main/java/me/tadho/markgo/App.java
+++ app/src/main/java/me/tadho/markgo/App.java
@@ -2,6 +2,8 @@ package me.tadho.markgo;
 
 import android.app.Application;
 
+import com.squareup.leakcanary.LeakCanary;
+
 import timber.log.Timber;
 
 /**
@@ -12,7 +14,15 @@ public class App extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        // Leak Canary init
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            // This process is dedicated to LeakCanary for heap analysis.
+            // You should not init your app in this process.
+            return;
+        }
+        LeakCanary.install(this);
 
+        // Timber init
         if (BuildConfig.DEBUG) {
             Timber.plant(new Timber.DebugTree());
         }
